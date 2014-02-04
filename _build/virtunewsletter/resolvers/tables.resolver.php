bb3298185623aeb47959e997dd94dc7ec0048c64
goldsky <goldsky@fastmail.fm>
2014-02-04 22:51:07 +0700

    fix table resolver to change db on selective upgrades

--- _build/virtunewsletter/resolvers/tables.resolver.php
+++ _build/virtunewsletter/resolvers/tables.resolver.php
@@ -25,13 +25,36 @@
  * @package virtunewsletter
  * @subpackage build
  */
+$php_ver_comp = version_compare(phpversion(), '5.3.0');
+if ($php_ver_comp < 0) {
+    return '<h1>FATAL ERROR: Setup cannot continue.</h1><p>Wrong PHP version! You\'re using PHP version ' . phpversion() . ', and virtuNewsletter requires version 5.3.0 or higher.</p>';
+}
+
 if ($modx = & $object->xpdo) {
+    $c = $modx->newQuery('transport.modTransportPackage');
+    $c->where(array(
+        'workspace' => 1,
+        "(SELECT
+            `signature`
+          FROM {$modx->getTableName('modTransportPackage')} AS `latestPackage`
+          WHERE `latestPackage`.`package_name` = `modTransportPackage`.`package_name`
+          ORDER BY
+             `latestPackage`.`version_major` DESC,
+             `latestPackage`.`version_minor` DESC,
+             `latestPackage`.`version_patch` DESC,
+             IF(`release` = '' OR `release` = 'ga' OR `release` = 'pl','z',`release`) DESC,
+             `latestPackage`.`release_index` DESC
+          LIMIT 1,1) = `modTransportPackage`.`signature`",
+    ));
+    $c->where(array(
+        'modTransportPackage.signature:LIKE' => '%virtunewsletter%',
+        'OR:modTransportPackage.package_name:LIKE' => '%virtunewsletter%',
+        'installed:IS NOT' => null
+    ));
+    $oldPackage = $modx->getObject('transport.modTransportPackage', $c);
+
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         case xPDOTransport::ACTION_INSTALL:
-            $php_ver_comp = version_compare(phpversion(), '5.3.0');
-            if ($php_ver_comp < 0) {
-                return '<h1>FATAL ERROR: Setup cannot continue.</h1><p>Wrong PHP version! You\'re using PHP version ' . phpversion() . ', and virtuNewsletter requires version 5.3.0 or higher.</p>';
-            }
             if ($modx->getDebug()) {
                 $modx->log(modX::LOG_LEVEL_WARN, 'resolver xPDOTransport::ACTION_INSTALL');
             }
@@ -49,6 +72,7 @@ if ($modx = & $object->xpdo) {
                 $manager->createObjectContainer('vnewsSubscribers');
                 $manager->createObjectContainer('vnewsSubscribersHasCategories');
             }
+
             break;
         case xPDOTransport::ACTION_UPGRADE:
             $modelPath = $modx->getOption('core_path') . 'components/virtunewsletter/model/';
@@ -58,41 +82,50 @@ if ($modx = & $object->xpdo) {
                 }
                 $manager = $modx->getManager();
 
-                $manager->addIndex('vnewsCategoriesHasUsergroups', 'fk_modx_virtunewsletter_categories_has_modx_virtunewsletter_idx');
-                $manager->addIndex('vnewsCategoriesHasUsergroups', 'fk_modx_virtunewsletter_categories_has_modx_virtunewsletter_idx1');
+                if ($oldPackage) {
 
-                $manager->addField('vnewsNewsletters', 'parent_id', array('after' => 'id'));
-                $manager->addIndex('vnewsNewsletters', 'parent_id');
+                    if ($oldPackage->compareVersion('1.6.0-beta1', '>')) {
+                        $manager->addIndex('vnewsCategoriesHasUsergroups', 'fk_modx_virtunewsletter_categories_has_modx_virtunewsletter_idx');
+                        $manager->addIndex('vnewsCategoriesHasUsergroups', 'fk_modx_virtunewsletter_categories_has_modx_virtunewsletter_idx1');
 
-                $manager->addIndex('vnewsNewslettersHasCategories', 'fk_modx_virtunewsletter_newsletters_has_modx_virtunewslette_idx');
-                $manager->addIndex('vnewsNewslettersHasCategories', 'fk_modx_virtunewsletter_newsletters_has_modx_virtunewslette_idx1');
+                        $manager->addField('vnewsNewsletters', 'parent_id', array('after' => 'id'));
+                        $manager->addIndex('vnewsNewsletters', 'parent_id');
 
-                // managing existing recurrences
-                $defaultVirtuNewsletterCorePath = $modx->getOption('core_path') . 'components/virtunewsletter/';
-                $virtuNewsletterCorePath = $modx->getOption('virtunewsletter.core_path', null, $defaultVirtuNewsletterCorePath);
-                $virtuNewsletter = $modx->getService('virtunewsletter', 'VirtuNewsletter', $virtuNewsletterCorePath . 'model/');
-                if ($virtuNewsletter instanceof VirtuNewsletter) {
-                    $newsletters = $modx->getCollection('vnewsNewsletters', array(
-                        'is_recurring' => 1,
-                        'is_active' => 1
-                    ));
-                    if ($newsletters) {
-                        foreach ($newsletters as $newsletter) {
-                            $virtuNewsletter->createNextRecurrence($newsletter->get('id'));
+                        $manager->addIndex('vnewsNewslettersHasCategories', 'fk_modx_virtunewsletter_newsletters_has_modx_virtunewslette_idx');
+                        $manager->addIndex('vnewsNewslettersHasCategories', 'fk_modx_virtunewsletter_newsletters_has_modx_virtunewslette_idx1');
+
+                        // managing existing recurrences
+                        $defaultVirtuNewsletterCorePath = $modx->getOption('core_path') . 'components/virtunewsletter/';
+                        $virtuNewsletterCorePath = $modx->getOption('virtunewsletter.core_path', null, $defaultVirtuNewsletterCorePath);
+                        $virtuNewsletter = $modx->getService('virtunewsletter', 'VirtuNewsletter', $virtuNewsletterCorePath . 'model/');
+                        if ($virtuNewsletter instanceof VirtuNewsletter) {
+                            $newsletters = $modx->getCollection('vnewsNewsletters', array(
+                                'is_recurring' => 1,
+                                'is_active' => 1
+                            ));
+                            if ($newsletters) {
+                                foreach ($newsletters as $newsletter) {
+                                    $virtuNewsletter->createNextRecurrence($newsletter->get('id'));
+                                }
+                            }
                         }
+
+                        $manager->removeField('vnewsReports', 'current_occurrence_time');
+                        $manager->removeField('vnewsReports', 'next_occurrence_time');
+                        $manager->addIndex('vnewsReports', 'fk_modx_virtunewsletter_reports_modx_virtunewsletter_newsle_idx');
+                        $manager->addIndex('vnewsReports', 'fk_modx_virtunewsletter_reports_modx_virtunewsletter_subscr_idx');
+
+                        $manager->removeIndex('vnewsSubscribers', 'user_id');
+                        $manager->addIndex('vnewsSubscribers', 'fk_modx_virtunewsletter_subscribers_modx_virtunewsletter_us_idx');
+
+                        $manager->addIndex('vnewsSubscribersHasCategories', 'fk_modx_virtunewsletter_subscribers_has_modx_virtunewslette_idx');
+                        $manager->addIndex('vnewsSubscribersHasCategories', 'fk_modx_virtunewsletter_subscribers_has_modx_virtunewslette_idx1');
+                    }
+
+                    if ($oldPackage->compareVersion('1.6.0-beta2', '>')) {
+                        $manager->alterField('vnewsNewsletters', 'scheduled_for');
                     }
                 }
-
-                $manager->removeField('vnewsReports', 'current_occurrence_time');
-                $manager->removeField('vnewsReports', 'next_occurrence_time');
-                $manager->addIndex('vnewsReports', 'fk_modx_virtunewsletter_reports_modx_virtunewsletter_newsle_idx');
-                $manager->addIndex('vnewsReports', 'fk_modx_virtunewsletter_reports_modx_virtunewsletter_subscr_idx');
-
-                $manager->removeIndex('vnewsSubscribers', 'user_id');
-                $manager->addIndex('vnewsSubscribers', 'fk_modx_virtunewsletter_subscribers_modx_virtunewsletter_us_idx');
-
-                $manager->addIndex('vnewsSubscribersHasCategories', 'fk_modx_virtunewsletter_subscribers_has_modx_virtunewslette_idx');
-                $manager->addIndex('vnewsSubscribersHasCategories', 'fk_modx_virtunewsletter_subscribers_has_modx_virtunewslette_idx1');
             }
             break;
         case xPDOTransport::ACTION_UNINSTALL:
